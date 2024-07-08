b6c6985980d497342a854117a6f4eb40d1926748
tzcake <892374682@qq.com>
2024-07-09 00:30:46 +0800

    feat：基于文件的路由系统

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- Nuxt自带基于文件的路由系统 -->
+    <!-- NuxtLink相当于RouterLink -->
+    <!-- NuxtPage相当于RouterView -->
+    <!-- 首页 index/index.vue -->
+    <NuxtLink to="/">首页</NuxtLink>
+    <!-- 视频页 video/index.vue -->
+    <NuxtLink to="/video">视频页</NuxtLink>
+    <!-- Nuxt 页面路由，相当于RouterView -->
+    <NuxtPage />
+  </div>
+</template>
