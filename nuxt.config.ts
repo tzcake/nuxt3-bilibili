440755dc6cce76ad4f318aa5c7d2428f0c9be892
tzcake <892374682@qq.com>
2024-07-10 16:02:27 +0800

    feat: 移动端适配

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   // 开启调试工具
   devtools: { enabled: true },
   // 应用模块
-  modules: ['@vant/nuxt']
-})
+  modules: ["@vant/nuxt"],
+  // 移动端适配
+  postcss: {
+    plugins: {
+      "postcss-px-to-viewport": {
+        viewportWidth: 375,
+      },
+    },
+  },
+});
